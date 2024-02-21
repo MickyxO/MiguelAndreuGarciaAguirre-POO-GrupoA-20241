48aeaf79894e5c19236e27e4dba6d1f0b8b0e107
Micky <miguelocho1lolazo@gmail.com>
2024-02-21 09:26:16 -0600

    Adding base.

---
+++ Base.java
@@ -0,0 +1,7 @@
+public class Base {
+    public static void main(String[] args) {
+        // Tu código comienza aquí
+        System.out.println("¡Hola, mundo!");
+        // Tu código termina aquí
+    }
+}
