d9e57f81d160dbbf315e033116c6216c01e20b87
Micky <miguelocho1lolazo@gmail.com>
2024-03-08 13:08:13 -0600

    Fixing develop branch.

--- Base.java
+++
@@ -1,7 +0,0 @@
-public class Base {
-    public static void main(String[] args) {
-        // Tu código comienza aquí
-        System.out.println("¡Hola, mundo!");
-        // Tu código termina aquí
-    }
-}
